5c3a74713d6bfadad27fbd93486797735dc55dd4
Saforomann <samuelforerom@gmail.com>
2025-01-10 09:32:13 -0500

    Add files via upload

---
+++ Debug/peripherals2.d
@@ -0,0 +1,69 @@
+# FIXED
+
+peripherals2.obj: ../peripherals2.c
+peripherals2.obj: ../peripherals.h
+peripherals2.obj: C:/ti/ccs1250/ccs/ccs_base/msp430/include/msp430.h
+peripherals2.obj: C:/ti/ccs1250/ccs/ccs_base/msp430/include/msp430f5529.h
+peripherals2.obj: C:/ti/ccs1250/ccs/ccs_base/msp430/include/in430.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+peripherals2.obj: C:/Users/samue/Mirror/SAM/WPI/Academy/Junior/C\ Term/ECE\ 2049/ECE2049_labs/ece2049_demo_ccs91/grlib/grlib.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+peripherals2.obj: C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/assert.h
+peripherals2.obj: ../LcdDriver/Sharp96x96.h
+peripherals2.obj: ../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h
+
+../peripherals2.c:
+
+../peripherals.h:
+
+C:/ti/ccs1250/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1250/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ti/ccs1250/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+C:/Users/samue/Mirror/SAM/WPI/Academy/Junior/C\ Term/ECE\ 2049/ECE2049_labs/ece2049_demo_ccs91/grlib/grlib.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+C:/ti/ccs1250/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/assert.h:
+
+../LcdDriver/Sharp96x96.h:
+
+../LcdDriver/HAL_MSP_EXP430FR5529_Sharp96x96.h:
+
